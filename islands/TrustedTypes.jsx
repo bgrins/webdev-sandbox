75bf18445a99af676a4ea269652576bfd5cac5fa
Brian Grinstead <briangrinstead@gmail.com>
2023-12-12 11:03:43 -0800

    more integration

--- islands/TrustedTypes.jsx
+++ islands/TrustedTypes.jsx
@@ -8,14 +8,44 @@ export default function TrustedTypes() {
   const container = useRef(null);
   useEffect(() => {
     const element = container.current;
+    if (window.trustedTypes && trustedTypes.createPolicy) { // Feature testing
+      let el = document.createElement('div');
+      element.append(el);
+      const escapeHTMLPolicy = trustedTypes.createPolicy('myEscapePolicy', {
+        createHTML: string => string.replace(/\</g, '&lt;')
+      });
+      const escaped = escapeHTMLPolicy.createHTML('<img src=x onerror=alert(1)>');
+      el.innerHTML = escaped;  // '&lt;img src=x onerror=alert(1)>'
+      console.log(escaped.toString());  // '<img src=x onerror=alert(1)>'
+
+      // https://developer.mozilla.org/en-US/docs/Web/API/TrustedTypePolicyFactory/createPolicy#createhtmlinputargs
+      trustedTypes.createPolicy("default", {
+        createScriptURL: (s, type, sink) => {
+          console.log("Please refactor.");
+          return `${s}?default-policy-used&type=${encodeURIComponent(
+            type,
+          )}&sink=${encodeURIComponent(sink)}`;
+        },
+
+        createHTML: string => string.replace(/\</g, '&lt;')
+      });
+
+      let script = document.createElement("script");
+      script.src = "https://example.com/my-script.js";
+      element.append(script);
+    }
+
+
+    const child = document.createElement("div");
+    element.append(child);
     const html = DOMPurify.sanitize(
       `<img src=x onerror="alert('XSS')"></img>`
     , {
       RETURN_TRUSTED_TYPE: true
     });
 
-    element.innerHTML = html.toString();
+    child.innerHTML = html.toString();
   }, [container]);
 
-  return <div><span ref={container}></span></div>;
+  return <div><div ref={container}></div></div>;
 }
--- routes/trustedtypes.tsx
+++ routes/trustedtypes.tsx
@@ -1,4 +1,19 @@
 import TrustedTypes from "../islands/TrustedTypes.jsx";
+import { HandlerContext } from "$fresh/server.ts";
+
+
+// Content-Security-Policy: require-trusted-types-for 'script'; report-uri //my-csp-endpoint.example
+
+
+export async function handler(
+  req: Request,
+  ctx: HandlerContext
+): Promise<Response> {
+  const resp = await ctx.render();
+  resp.headers.set("Content-Security-Policy", "trusted-types myEscapePolicy dompurify default; require-trusted-types-for 'script'; report-uri /my-csp-endpoint");
+  return resp;
+}
+
 
 // Check Header Sec-Purpose: prefetch;prerender
 // Link to the sleep page - including with a param to allow caching
